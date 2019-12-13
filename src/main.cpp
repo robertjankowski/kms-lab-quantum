2cb0c16538da100ba04a3a565ec14c52d2cb66f6
robertjankowski <robjanw11@wp.pl>
2019-12-13 19:18:37 +0100

    add raport

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,12 @@ std::string getCustomFilename(std::string_view basePath, std::string_view filena
 
 int main() {
     std::string_view basePath = "../output";
-    const auto kappa = 1;
-    auto step{14.8};
+    const auto kappa = 9;
+    auto step{10};
     for (int i = 0; i < 1; ++i) {
         Simulation simulation("../input/parameters.txt");
-        const std::string metricsFile = getCustomFilename(basePath, "metrics_resonance", kappa, step, 1);
-        const std::string densityFile = getCustomFilename(basePath, "density_resonance", kappa, step, 1);
+        const std::string metricsFile = getCustomFilename(basePath, "metrics_no_resonance", kappa, step, 1);
+        const std::string densityFile = getCustomFilename(basePath, "density_no_resonance", kappa, step, 1);
         simulation.setOmega(step);
         simulation.run(metricsFile, densityFile);
         step += 0.1;
